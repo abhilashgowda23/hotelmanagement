2245c558da9fc37c339d87f8e3cf819b6df3213a
Preethi R <37240088+preethi905@users.noreply.github.com>
2024-07-13 18:50:31 +0530

    Update Guest.java

--- src/main/java/Guest.java
+++ src/main/java/Guest.java
@@ -1,2 +1,32 @@
+package hotelmanagement;
+
 public class Guest {
+  private final String id;
+  private final String name;
+  private final String email;
+  private final String phonenumber;
+
+  public Guest(String id, String name, String email, String phonenumber){
+    this.id = id;
+    this.name = name;
+    this.email = email;
+    this.phonenumber = phonenumber;
+  }
+
+  public String getId(){
+    return id;
+  }
+
+  public String getName(){
+    return name;
+  }
+
+  public String getEmail(){
+    return email;
+  }
+
+  public String getPhoneNumber(){
+    return phonenumber;
+  }
+                    
 }
